d4479d69485ce3a6a067ca925f8030aa2454a62b
Nicole2512 <nicolemokaya@gmail.com>
2022-06-20 09:52:20 -0700

    Bi. bug fix

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
+# 5-raise_exception.py
 def raise_exception():
-    try:
-        raise(TypeError)
-    except:
-        raise
+    """Raise a TypeError exception."""
+    raise TypeError
